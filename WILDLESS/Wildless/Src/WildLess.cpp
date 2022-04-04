48aeb928a1ebb8d8e5165629e7d6932d22b5fcae
Agusmoure <amoure@ucm.es>
2022-04-04 21:11:39 +0200

    Empezada dll

---
+++ WILDLESS/Wildless/Src/WildLess.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "Wildless.h"
+//WildLess::WildLess()
+//{
+//}
+//
+//WildLess::~WildLess()
+//{
+//}
+int WildLess::firstTry() {
+	std::cout << "\nME CAGO EN TU PUTA MADRE\n";
+	return 0;
+}
---
+++ WILDLESS/Wildless/Src/WildLess.h
@@ -0,0 +1,17 @@
+#ifndef WildLessSrc
+#define WildLessSrc
+#include "WildlessDll.h"
+class WILDLESS_API WildLess
+{
+public:
+	 WildLess() {};
+	virtual ~WildLess() {};
+
+	virtual int firstTry();
+
+private:
+
+};
+
+
+#endif // !WildLess
