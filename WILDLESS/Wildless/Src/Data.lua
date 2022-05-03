3a02d9970d1ab9a317533cd9a26b49779ff07fb8
Agusmoure <amoure@ucm.es>
2022-05-03 22:59:01 +0200

    Bat del juego

--- WILDLESS/Wildless/Src/Data.lua
+++ WILDLESS/Wildless/Src/Data.lua
@@ -1,7 +1,7 @@
 --DATA
 -- Insert the name of the DLL without de extension Example if the DLL generated is ExampleDLL.dll then write ExampleDLL
 function DLLName()
- return "ExampleDLL"
+ return "WildLess_d"
 end
 -- Insert the name of the function that start the game, remember this function must not be a part of a class and it has to be writing as a extern C
 --If the function is the next
@@ -12,10 +12,17 @@ end
 --} 
 -- Then write exampleFunc
 function FunctionName()
-    return "ExampleFunc"
+    return "firstTry"
    end
 -- Insert the name you want on the window
 function WindowName()
     return "WindowName"
 end
-
+--Insert the name of the first scene file to load
+function SceneFile()
+    return "Game.lua"
+end
+--insert the name the name of the scene
+function SceneName()
+    return "game"
+end
